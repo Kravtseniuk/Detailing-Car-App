931331148fbf413308c59cbf10ee7c81a9dc6fe4
Ihor <kravtseniuk@gmail.com>
2024-04-27 16:04:47 +0300

    [add] Data Transfer Object for Log/Reg

---
+++ BaseLibrary/DTOs/Register.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.DTOs
+{
+    public class Register : AccountBase
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+        
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string? SecondName { get; set; }
+        
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Required]
+        public string? ConfirmPassword { get; set; }
+    }
+}

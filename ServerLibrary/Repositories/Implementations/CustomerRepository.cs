f61fd94be6ed0114ed451a87f9ee77e4a9e53ad1
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2026-03-15 01:30:42 +0200

    [add] CustomerRepository

---
+++ ServerLibrary/Repositories/Implementations/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using BaseLibrary.Entities;
+using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class CustomerRepository(AppDbContext appDbContext) : IGenericRepositoryInterface<Customer>
+    {
+        public Task<GeneralResponse> DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Customer>> GetAll() => await appDbContext.Customers
+            .Include(c => c.Orders)
+            .ToListAsync();
+
+        public Task<Customer> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse> Insert(Customer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse> Update(Customer item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

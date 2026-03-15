b5ed65f3afc9d3a01bafe935df8ff7bb9caac627
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2026-03-15 02:30:48 +0200

    Update Employee.cs

--- BaseLibrary/Entities/Employee.cs
+++ BaseLibrary/Entities/Employee.cs
@@ -5,11 +5,20 @@ namespace BaseLibrary.Entities
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
         
         [Required]
-        public string Fullname { get; set; } = string.Empty;
+        public string SecondName { get; set; } = string.Empty;
         
-        [Required]
+        public string LastName { get; set; } = string.Empty;
+        
+        [Required, Phone]
         public string TelephoneNumber { get; set; } = string.Empty;
+
+        public ICollection<Order> ExecutedOrders { get; set; } = new List<Order>();
+
+        public string FullName => $"{SecondName} {FirstName} {LastName}".Trim();
     }
 }

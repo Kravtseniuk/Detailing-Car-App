7a27844cc7ced6b274bb2f7bb1a5fb73e947ca3c
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2026-04-03 02:41:11 +0300

    [add] IGenericServiceInterface

---
+++ ClientLibrary/Services/Contracts/IGenericServiceInterface.cs
@@ -0,0 +1,21 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.DTOs.Order;
+using BaseLibrary.Responses;
+
+namespace ClientLibrary.Services.Contracts
+{
+    public interface IGenericServiceInterface<T>
+    {
+        Task<List<T>> GetAll(string baseUrl);
+
+        Task<GeneralResponse> InsertOrder(T item, string baseUrl);
+        Task<List<EmployeeDto>> GetAllEmployeesAsync(string baseUrl);
+        Task<List<GetOrdersDto>> GetAllOrdersAsync(string baseUrl);
+        Task<List<CompanyServiceDto>> GetCompanyServiceForDropdown(string baseUrl);
+
+        Task<T> GetById(int id, string baseUrl);
+        Task<GeneralResponse> Insert(T item, string baseUrl);
+        Task<GeneralResponse> Update(T item, string baseUrl);
+        Task<GeneralResponse> DeleteById(int id, string baseUrl);
+    }
+}

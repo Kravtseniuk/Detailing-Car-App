f74763de67c7bfc492262d481d3b9caf76002084
Ihor <106812312+Kravtseniuk@users.noreply.github.com>
2026-04-03 03:01:19 +0300

    [add] Dashboard and content pages

---
+++ Client/ApplicationStates/AllState.cs
@@ -0,0 +1,30 @@
+﻿namespace Client.ApplicationStates
+{
+    public class AllState
+    {
+        public Action? Action { get; set; }
+
+        public bool ShowCompanyServices { get; set; }
+        public bool ShowEmployee { get; set; }
+
+        public void CompanyServicesClicked()
+        {
+            ResetAllDepartments();
+            ShowCompanyServices = true;
+            Action?.Invoke();
+        }
+
+        public void EmployeeClicked()
+        {
+            ResetAllDepartments();
+            ShowEmployee = true;
+            Action?.Invoke();
+        }
+
+        private void ResetAllDepartments()
+        {
+            ShowCompanyServices = false;
+            ShowEmployee = false;
+        }
+    }
+}
